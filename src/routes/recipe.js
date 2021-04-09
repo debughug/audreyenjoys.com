36c2d423267f0b725f0c1303b1346a2537b4e9d1
Richard Anderson <debughug@gmail.com>
2021-04-08 20:00:41 -0700

    Converted Recipe comp to stateless and vendor file move

--- src/routes/recipe.js
+++ src/routes/recipe.js
@@ -1,78 +1,80 @@
-import AwesomeSlider from 'react-awesome-slider';
-import withAutoplay from 'react-awesome-slider/dist/autoplay';
-import React from 'react';
+import AwesomeSlider from "react-awesome-slider";
+import withAutoplay from "react-awesome-slider/dist/autoplay";
+import React from "react";
 
 const AutoplaySlider = withAutoplay(AwesomeSlider);
-class Recipe extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      recipe: props.recipe,
-    };
-  }
+let Recipe = ({ recipe }) => {
+  window.scrollTo(0, 0);
+  let currentFood = recipe;
+  let instagramVideoLink = currentFood.recipeVideoShareLink;
+  let recipeSliderURLS = currentFood.recipeSliderURLS || [];
+  let allRecipeImages = [currentFood.recipeImageURL].concat(recipeSliderURLS);
+  let hasRecipeSlider = recipeSliderURLS.length > 0;
 
-  componentDidMount() {
-    window.scrollTo(0, 0);
-  }
+  let MainAsset = hasRecipeSlider ? (
+    <AutoplaySlider
+      className="food-asset carousel"
+      bullets={false}
+      play={true}
+      interval={3210}
+    >
+      {allRecipeImages.map((url) => (
+        <div data-src={url} />
+      ))}
+    </AutoplaySlider>
+  ) : (
+    <div
+      className="food-asset image"
+      style={{ backgroundImage: `url(${currentFood.recipeImageURL})` }}
+    ></div>
+  );
 
-  render() {
-    let currentFood = this.state.recipe;
-    let instagramVideoLink = currentFood.recipeVideoShareLink;
-    let recipeSliderURLS = currentFood.recipeSliderURLS || [];
-    let allRecipeImages = [currentFood.recipeImageURL].concat(recipeSliderURLS);
-    let hasRecipeSlider = recipeSliderURLS.length > 0;
-
-    let MainAsset = hasRecipeSlider ? (
-      <AutoplaySlider className="food-asset carousel" bullets={false} play={true} interval={3210}>
-        {allRecipeImages.map(url => (
-          <div data-src={url} />
-        ))}
-      </AutoplaySlider>
-    ) : (
-      <div className="food-asset image" style={{ backgroundImage: `url(${currentFood.recipeImageURL})` }}></div>
-    );
-
-    return (
-      <div className={`Viewer trasition-base`} style={{ backgroundColor: currentFood.inlineColor }}>
-        <div className="content">
-          <div className="intro">
-            <div className="food-intro">
-              <h6 className="food-brand">AudreyEnjoys</h6>
-              <h1 className="food-name">{currentFood.recipeName}</h1>
-              <div className="social-links">
-                {instagramVideoLink && (
-            <a href={instagramVideoLink}>
-              <i className="fab fa-instagram"></i>
-              <span className="instagram-label">Watch on Instagram</span>
-            </a>
-                )}
-              </div>
+  
+  return (
+    <div
+      className={`Viewer trasition-base`}
+      style={{ backgroundColor: currentFood.inlineColor }}
+    >
+      <div className="content">
+        <div className="intro">
+          <div className="food-intro">
+            <h6 className="food-brand">AudreyEnjoys</h6>
+            <h1 className="food-name">{currentFood.recipeName}</h1>
+            <div className="social-links">
+              {instagramVideoLink && (
+                <a href={instagramVideoLink}>
+                  <i className="fab fa-instagram"></i>
+                  <span className="instagram-label">Watch on Instagram</span>
+                </a>
+              )}
             </div>
-            {MainAsset}
           </div>
-          <div className="body">
-            <div className="ingredients">
-              <h4>Ingredients</h4>
-              <ul className="ingredients-list">
-                {currentFood.recipeIngredients.map(entry => (
-                  <li>{entry}</li>
-                ))}
-              </ul>
-            </div>
-            <div className="instructions">
-              <h4>Instructions</h4>
-              <ol className='instructions-list'>
-                {currentFood.recipeInstructions.map(entry => (
-                  <li>{entry}</li>
-                ))}
-              </ol>
-            </div>
+          {MainAsset}
+        </div>
+        <div className="body">
+          <div className="ingredients">
+            <h4>Ingredients</h4>
+            <ul className="ingredients-list">
+              {currentFood.recipeIngredients.map((entry) => (
+                <li>{entry}</li>
+              ))}
+            </ul>
+          </div>
+          <div className="instructions">
+            <h4>Instructions</h4>
+            <ol className="instructions-list">
+              {currentFood.recipeInstructions.map((entry) => (
+                <li>{entry}</li>
+              ))}
+            </ol>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  ); 
+};
+
+
 
 export default Recipe;
