e6348bedec35af68fefffc2496adaf54894dbc2a
Richard Anderson <debughug@gmail.com>
2021-04-23 18:16:50 -0700

    Ingridients and Instructions are checklists now

--- src/routes/recipe.js
+++ src/routes/recipe.js
@@ -12,7 +12,8 @@ let InstagramLink = ({ instagramVideoLink }) => {
     element = (
       <a href={instagramVideoLink} target="_blank">
         <i className="fab fa-instagram"></i>
-        <span className="instagram-label">Watch on Instagram</span><i class="fas fa-external-link-alt"></i>
+        <span className="instagram-label">Watch on Instagram</span>
+        <i className="fas fa-external-link-alt"></i>
       </a>
     );
   }
@@ -20,74 +21,138 @@ let InstagramLink = ({ instagramVideoLink }) => {
   return element;
 };
 
-let Recipe = ({ recipe }) => {
-  window.scrollTo(0, 0);
+class Recipe extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div
-      className={`route-recipe`}
-      style={{ backgroundColor: recipe.inlineColor }}
-    >
-      <div className="content">
-        <div className="intro">
-          <div className="food-intro">
-            <h6>AudreyEnjoys</h6>
-            <h1>{recipe.recipeName}</h1>
-            <div className="social-links">
-              <InstagramLink
-                instagramVideoLink={recipe.recipeVideoShareLink}
-              ></InstagramLink>
+    let checkedIngredients = [];
+
+    for (let x = 0; x < this.props.recipe.recipeInstructions.length; x++) {
+      checkedIngredients.push(false);
+    }
+
+    let checkedInstructions = [];
+
+    for (let x = 0; x < this.props.recipe.recipeInstructions.length; x++) {
+      checkedInstructions.push(false);
+    }
+
+    this.state = {
+      recipe: this.props.recipe,
+      checkedIngredients,
+      checkedInstructions,
+    };
+
+    this.toggleIngredientsCheckClass = this.toggleIngredientsCheckClass.bind(
+      this
+    );
+
+    this.toggleInstructionsCheckClass = this.toggleInstructionsCheckClass.bind(
+      this
+    );
+  }
+
+  toggleIngredientsCheckClass(index) {
+    let checkedIngredients = this.state.checkedIngredients;
+    checkedIngredients[index] = !checkedIngredients[index];
+    this.setState({ checkedIngredients });
+  }
+
+  toggleInstructionsCheckClass(index) {
+    let checkedInstructions = this.state.checkedInstructions;
+    checkedInstructions[index] = !checkedInstructions[index];
+    this.setState({ checkedInstructions });
+  }
+
+  render() {
+    let recipe = this.state.recipe;
+
+    return (
+      <div
+        className={`route-recipe`}
+        style={{ backgroundColor: recipe.inlineColor }}
+      >
+        <div className="content">
+          <div className="intro">
+            <div className="food-intro">
+              <h6>AudreyEnjoys</h6>
+              <h1>{recipe.recipeName}</h1>
+              <div className="social-links">
+                <InstagramLink
+                  instagramVideoLink={recipe.recipeVideoShareLink}
+                ></InstagramLink>
+              </div>
+            </div>
+            <AutoplaySlider
+              infinite={recipe.allRecipeImagesURLS.length > 0}
+              bullets={false}
+              play={true}
+              interval={3210}
+            >
+              {recipe.allRecipeImagesURLS.map((url, index) => (
+                <div key={index} data-src={url} />
+              ))}
+            </AutoplaySlider>
+          </div>
+          {recipe.metaInfo.length && (
+            <div className="metas">
+              <h4>Overview</h4>
+              {recipe.metaInfo.map((data, index) => (
+                <div className="meta" key={index}>
+                  <span className="key">{data.label}:</span>
+                  <span className="value">{data.displayValue}</span>
+                </div>
+              ))}
+            </div>
+          )}
+          <div className="body">
+            <div className="ingredients">
+              <h4>Ingredients</h4>
+              <ul className="ingredients-list cursor">
+                {recipe.recipeIngredients.map((entry, index) => (
+                  <li
+                    className={
+                      this.state.checkedIngredients[index]
+                        ? "strikethrough"
+                        : ""
+                    }
+                    onClick={() => this.toggleIngredientsCheckClass(index)}
+                    key={index}
+                  >
+                    {entry}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="instructions">
+              <h4>Instructions</h4>
+              <ol className="instructions-list cursor">
+                {recipe.recipeInstructions.map((entry, index) => (
+                  <li
+                    className={
+                      this.state.checkedInstructions[index]
+                        ? "strikethrough"
+                        : ""
+                    }
+                    onClick={() => this.toggleInstructionsCheckClass(index)}
+                    key={index}
+                  >
+                    {entry}
+                  </li>
+                ))}
+              </ol>
             </div>
           </div>
-          <AutoplaySlider
-            infinite={recipe.allRecipeImagesURLS.length > 0}
-            bullets={false}
-            play={true}
-            interval={3210}
-          >
-            {recipe.allRecipeImagesURLS.map((url, index) => (
-              <div key={index} data-src={url} />
-            ))}
-          </AutoplaySlider>
+          {recipe.notes && (
+            <div className="notes">
+              <h4>Notes</h4>
+              {documentToReactComponents(recipe.notes)}
+            </div>
+          )}
         </div>
-        {recipe.metaInfo.length && (
-          <div className="metas">
-            <h4>Overview</h4>
-            {recipe.metaInfo.map((data, index) => (
-              <div className="meta" key={index}>
-                <span className="key">{data.label}:</span>
-                <span className="value">{data.displayValue}</span>
-              </div>
-            ))}
-          </div>
-        )}
-        <div className="body">
-          <div className="ingredients">
-            <h4>Ingredients</h4>
-            <ul className="ingredients-list">
-              {recipe.recipeIngredients.map((entry, index) => (
-                <li key={index}>{entry}</li>
-              ))}
-            </ul>
-          </div>
-          <div className="instructions">
-            <h4>Instructions</h4>
-            <ol className="instructions-list">
-              {recipe.recipeInstructions.map((entry, index) => (
-                <li key={index}>{entry}</li>
-              ))}
-            </ol>
-          </div>
-        </div>
-        {recipe.notes && (
-          <div className="notes">
-            <h4>Notes</h4>
-            {documentToReactComponents(recipe.notes)}
-          </div>
-        )}
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Recipe;
