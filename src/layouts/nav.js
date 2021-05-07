0eb62112c3b116cbb916a3719f212d4ebde356d1
Richard Anderson <debughug@gmail.com>
2021-05-07 16:59:08 -0700

    Got rid of rushed top-nav code

--- src/layouts/nav.js
+++ src/layouts/nav.js
@@ -1,28 +1,30 @@
 import React from "react";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 
-let Home = ({ recipes }) => {
+let Home = () => {
   return (
     <div className="top-nav">
-      <div className="nav">
-        <div className="logo">
-          <img
-            src="https://audreyenjoys.s3-us-west-2.amazonaws.com/logo-icon.jpg"
-            alt="AudreyEnjoys Profile"
-          />
+      <div class="container">
+        <div className="nav">
+          <div className="logo">
+            <img
+              src="https://audreyenjoys.s3-us-west-2.amazonaws.com/logo-icon.jpg"
+              alt="AudreyEnjoys Profile"
+            />
+          </div>
+          <h1>AudreyEnjoys</h1>
+          <div className="links">
+            <Link to="/">Recipes</Link>
+          </div>
         </div>
-        <h1>AudreyEnjoys</h1>
-        <div className="links">
-          <Link to="/">Recipes</Link>
+        <div className="social-links">
+          <a href="mailto:me@audreyenjoys.com" className="email-link">
+            <i className="far fa-envelope"></i>
+          </a>
+          <a href="https://www.instagram.com/audreyenjoys/">
+            <i className="fab fa-instagram"></i>
+          </a>
         </div>
-      </div>
-      <div className="social-links">
-        <a href="mailto:me@audreyenjoys.com" className="email-link">
-          <i className="far fa-envelope"></i>
-        </a>
-        <a href="https://www.instagram.com/audreyenjoys/">
-          <i className="fab fa-instagram"></i>
-        </a>
       </div>
     </div>
   );
