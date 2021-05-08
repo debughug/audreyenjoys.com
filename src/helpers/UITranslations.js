1cac8a4781ed21efe4a86d738d259293e527a788
Richard Anderson <debughug@gmail.com>
2021-05-08 14:45:59 -0700

    UI Translations

---
+++ src/helpers/UITranslations.js
@@ -0,0 +1,34 @@
+let UITranslations = {
+  en: {
+    Recipes: "Recipes",
+    Overview: "Overview",
+    "Preparation Time": "Preparation Time",
+    "Cook Time": "Cook Time",
+    day: "day",
+    hour: "hour",
+    minute: "minute",
+    "Serving Size": "Serving Size",
+    Friends: "Friends",
+    Difficulty: "Difficulty",
+    Ingredients: "Ingredients",
+    Instructions: "Instructions",
+    Notes: "Notes",
+  },
+  es: {
+    Recipes: "Recetas",
+    Overview: "Descripción general",
+    "Preparation Time": "Tiempo de preparación",
+    "Cook Time": "Hora de cocinar",
+    day: "día",
+    hour: "hora",
+    minute: "minuto",
+    "Serving Size": "Tamaño de la porción",
+    Friends: "Amigos",
+    Difficulty: "Dificultad",
+    Ingredients: "ingredientes",
+    Instructions: "Instrucciones",
+    Notes: "Notas",
+  },
+};
+
+export default UITranslations;
