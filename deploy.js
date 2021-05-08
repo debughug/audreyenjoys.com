48d2fb27361317640e28167b6a50ec62db0557f6
Richard Anderson <debughug@gmail.com>
2021-05-08 13:14:04 -0700

    Updated deploy.js to update the ES file too.

--- deploy.js
+++ deploy.js
@@ -1,24 +1,40 @@
-require('dotenv').config({ path: __dirname + '/.env' });
+require("dotenv").config({ path: __dirname + "/.env" });
 
-const { upload } = require('s3-lambo');
-const axios = require('axios');
+const { upload } = require("s3-lambo");
+const axios = require("axios");
 
 const UpdateS3Recipes = async () => {
+  let englishCall = axios.get(
+    `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master/entries?content_type=recipe&access_token=${process.env.CONTENTFUL_DELIVERY_KEY}`
+  );
+
+  let spanishCall = axios.get(
+    `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master/entries?content_type=recipe&access_token=${process.env.CONTENTFUL_DELIVERY_KEY}&locale=es`
+  );
+
   await axios
-    .get(
-      `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master/entries?content_type=recipe&access_token=${process.env.CONTENTFUL_DELIVERY_KEY}`
+    .all([englishCall, spanishCall])
+    .then(
+      axios.spread(function (englishRes, spanishRes) {
+        upload({
+          ACL: "public-read",
+          Key: "recipes.json",
+          Bucket: process.env.AWS_BUCKET_NAME,
+          Body: JSON.stringify(englishRes.data),
+        });
+
+        upload({
+          ACL: "public-read",
+          Key: "recipes-es.json",
+          Bucket: process.env.AWS_BUCKET_NAME,
+          Body: JSON.stringify(spanishRes.data),
+        });
+
+        console.log("Successfully Updated S3 Files! :)");
+      })
     )
-    .then(function (response) {
-      upload({
-        ACL: 'public-read',
-        Key: 'recipes.json',
-        Bucket: process.env.AWS_BUCKET_NAME,
-        Body: JSON.stringify(response.data),
-      });
-      console.log('Updated recipe site database, thank you!');
-    })
     .catch(function (error) {
-      console.log('Failed to update recipe site database.', error);
+      console.log("Unsuccessfully Updated S3 Files! :(", error);
     });
 };
 
