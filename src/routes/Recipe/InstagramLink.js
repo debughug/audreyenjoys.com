68d5759d2dfdfcd73fd3931f8745c080fd4d978a
Richard Anderson <debughug@gmail.com>
2021-06-02 17:48:57 -0700

    Bug: Removed Instagram Links that didn't actually exist

--- src/routes/Recipe/InstagramLink.js
+++ src/routes/Recipe/InstagramLink.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 let InstagramLink = ({ instagramVideoLink }) => {
-  return (
-    <a href={instagramVideoLink} target="_blank" rel="noopener noreferrer">
-      <i className="fab fa-instagram"></i>
-    </a>
-  );
+  let element = null;
+
+  if (instagramVideoLink) {
+    element = (
+      <a href={instagramVideoLink} target="_blank" rel="noopener noreferrer">
+        <i className="fab fa-instagram"></i>
+      </a>
+    );
+  }
+
+  return element;
 };
 
 export default InstagramLink;
